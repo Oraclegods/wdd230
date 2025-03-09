d850bf07abd50569d8a155cf5573700e22a8c993
Oraclegods <58555206+Oraclegods@users.noreply.github.com>
2025-03-09 12:10:04 +0100

    Update getDates.js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,4 @@
+// Dynamically update the copyright year and last modified date
 
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
