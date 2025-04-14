390902e8bd5501087bf6c5cac0947579ee2ac792
Oraclegods <58555206+Oraclegods@users.noreply.github.com>
2025-04-14 12:22:37 +0100

    Update weather.js

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,1 +1,37 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// Coordinates for Trier, Germany: approx. 49.75°N, 6.64°E
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=YOUR_API_KEY_HERE';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // Testing only
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;F`;
+
+  const iconCode = data.weather[0].icon;
+  const iconsrc = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  let desc = data.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', `Weather: ${desc}`);
+  captionDesc.textContent = desc;
+}
+
+apiFetch();
 
